e2e0389c028ecd7ba239a3cceb92253a82aeaede
TOBYBorysionek <gameboys1210@gmail.com>
2023-11-09 10:28:00 +0100

    lekcja

---
+++ samochod.py
@@ -0,0 +1,8 @@
+class samochod:
+    def __init__(self, marka, model, rocznik, pojemnosc_silnika, przebieg, wlasciciel):
+        self.marka = marka
+        self.model = model
+        self.rocznik = rocznik
+        self.pojemnosc_silnika = pojemnosc_silnika
+        self.przebieg = przebieg
+        self.wlasciciel = wlasciciel
