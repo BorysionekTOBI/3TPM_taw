315065d7441eb68d2aa3b00ee3387e86410b6f5a
unknown <tobibobi547@gmail.com>
2023-11-12 17:47:13 +0100

    zadanie

--- m_zapis_odczyt.py
+++ m_zapis_odczyt.py
@@ -19,3 +19,35 @@ def zapis(nazwapliku, tryb, rodzaj, tresc):
         print("Problem z dostępem do pliku...")
     except Exception as e:
         print(f"Wystąpił bład {e}...")
+
+
+
+def odczytaj(nazwapliku, rodzaj):
+        try:
+            with open(nazwapliku, 'r') as f:
+                if rodzaj == "txt":
+                    data = f.read()
+                elif rodzaj == "lista":
+                    data = [line.strip() for line in f]
+                elif rodzaj == "slownik":
+                    data = {}
+                    for line in f:
+                        key, value = line.strip().split(' : ')
+                        data[key] = value
+                else:
+                    print(f"Nie potrafię odczytać pliku z danymi: {rodzaj}.")
+                return None
+                return data
+        except FileNotFoundError:
+            print("Problem z dostępem do pliku...")
+            return None
+        except Exception as e:
+            print(f"Wystąpił błąd {e}...")
+            return None
+
+nazwapliku = "sample.txt"
+rodzaj = "txt"
+dane = odczytaj(nazwapliku, rodzaj)
+if dane is not None:
+    print(f"Odczytano dane z pliku {nazwapliku}:")
+    print(dane)
